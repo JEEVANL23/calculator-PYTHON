7a3d3f3c07f1b81f61118cb8655f75d5cbc5b103
Jeevan <57611618+jeevandowney@users.noreply.github.com>
2020-05-24 21:41:56 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -17,7 +17,7 @@ print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
 # Take input from the user 
-choice = input("Enter choice(1/2/3/4/5): ")
+choice = input("Enter choice(1/2/3/4): ")
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 if choice == '1':
